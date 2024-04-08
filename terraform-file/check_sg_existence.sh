3e758b7a0fc1162f4f26cd1547b863fb36d63709
krosand <krosand@gmail.com>
2024-04-09 00:52:30 +0200

    test script

--- terraform-file/check_sg_existence.sh
+++ terraform-file/check_sg_existence.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Replacez eu-west-3 par votre région AWS si nécessaire
 GROUP_EXISTS=$(aws ec2 describe-security-groups --filters Name=group-name,Values='Deployment-server-petclinic' --region eu-west-3 --query 'SecurityGroups[*].GroupId' --output text)
 
 if [ -z "$GROUP_EXISTS" ]; then
-  echo "Security group does not exist, setting create_sg to true."
-  export TF_VAR_create_sg=true
+  echo "create_sg=true" >> $GITHUB_ENV
 else
-  echo "Security group already exists, setting create_sg to false."
-  export TF_VAR_create_sg=false
+  echo "create_sg=false" >> $GITHUB_ENV
 fi
 
