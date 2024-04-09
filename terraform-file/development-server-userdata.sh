ce200a93db2f6af3c9d2e10df5ba928876ebe212
krosand <krosand@gmail.com>
2024-04-09 04:35:04 +0200

    tempo script

--- terraform-file/development-server-userdata.sh
+++ terraform-file/development-server-userdata.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 sleep 30
 echo test
-sudo yum update -y
-sudo hostnamectl set-hostname Development-Server
+sudo yum install java-11-amazon-corretto -y
+git clone https://github.com/krosand/spring-petclinic-cloud.git
+cd spring-petclinic-cloud
+git checkout main
+
+hostnamectl set-hostname Development-Server
 sudo amazon-linux-extras install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
@@ -10,8 +14,3 @@ sudo usermod -a -G docker ec2-user
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-sudo yum install git -y
-sudo yum install java-11-amazon-corretto -y
-git clone https://github.com/krosand/spring-petclinic-cloud.git
-cd spring-petclinic-cloud
-git checkout main
