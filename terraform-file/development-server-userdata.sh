8844a14da9bcc318f6917175edb6f4df87701939
krosand <krosand@gmail.com>
2024-04-09 03:53:28 +0200

    perm script

--- terraform-file/development-server-userdata.sh
+++ terraform-file/development-server-userdata.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
-yum update -y
-hostnamectl set-hostname Development-Server
-amazon-linux-extras install docker -y
-systemctl start docker
-systemctl enable docker
-usermod -a -G docker ec2-user
+sudo yum update -y
+sudo hostnamectl set-hostname Development-Server
+sudo amazon-linux-extras install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -a -G docker ec2-user
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-yum install git -y
-yum install java-11-amazon-corretto -y
+sudo yum install git -y
+sudo yum install java-11-amazon-corretto -y
 git clone https://github.com/krosand/spring-petclinic-cloud.git
 cd spring-petclinic-cloud
 git checkout main
